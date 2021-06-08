7d20582e81eeec75799338687b359f8131a569c7
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-06-07 19:28:54 -0500

    changes to more testable class

--- lib/data_source/quotes_api.dart
+++ lib/data_source/quotes_api.dart
@@ -1,15 +1,24 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:quotes_app/models/quote.dart';
 import 'package:quotes_app/logic/quotes.dart';
 
 class QuotesAPI extends QuotesDataSource {
+
+  var client = http.Client();
+
   @override
   Future<Quote> getRandomQuote() async {
+    return requestRandomQuote(client);
+  }
+
+  Future<Quote> requestRandomQuote(http.Client client) async {
     final response =
-        await http.get(Uri.parse("https://quotes.rest/qod?language=en"));
+        await client.get(Uri.parse("https://quotes.rest/qod?language=en"));
     if (response.statusCode == 200) {
+      print(response.body);
       final Map<String, dynamic> data = jsonDecode(response.body);
       final Map<String, dynamic> quoteData = data['contents']['quotes'][0];
       return Quote.fromJson(quoteData);
